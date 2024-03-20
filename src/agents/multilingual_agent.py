c7fa09d8cfc91a513b1fccdd83dbe08a4aaa042d
Lenin Kennedy <leninkennedy4work@gmail.com>
2024-03-20 22:53:21 +0530

    feat: agent

--- src/agents/multilingual_agent.py
+++ src/agents/multilingual_agent.py
@@ -1,9 +1,13 @@
 # Necessary imports: uagents for agent creation and message handling,
 # os and requests for managing API calls
-from uagents import Agent, Context
+from uagents import Agent, Context, Protocol, Model
+from pydantic import Field
+from ai_engine import UAgentResponse, UAgentResponseType
+from messages.basic import UAResponse, UARequest, Error
 from uagents.setup import fund_agent_if_low
 import os
 import requests
+from utils.functions import get_video_script, summarize_transcript
 
 # The access token and URL for the SAMSUM BART model, served by Hugging Face
 HUGGING_FACE_ACCESS_TOKEN = os.getenv(
@@ -17,38 +21,77 @@ HEADERS = {
 
 SEED=HUGGING_FACE_ACCESS_TOKEN
 
-# Creating the agent and funding it if necessary
+
+# Copy the address shown below
+print(f"Your agent's address is: {Agent(seed=SEED).address}")
+
+
+# Then go to https://agentverse.ai, register your agent in the Mailroom
+# and copy the agent's mailbox key
+AGENT_MAILBOX_KEY = os.getenv("AGENT_MAILBOX_KEY", None)
+assert (
+    AGENT_MAILBOX_KEY is not None
+), "AGENT_MAILBOX_KEY environment variable is not set! Please set it to your agent's mailbox key!"
+
+# Now your agent is ready to join the agentverse!
 agent = Agent(
     name="multilingual-agent",
     seed=SEED,
-    port=8001,
-    endpoint=["http://127.0.0.1:8001/submit"],
+    mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
 )
-fund_agent_if_low(agent.wallet.address())
 
-@agent.on_event("startup")
-async def on_start(ctx: Context):
-    ctx.logger.info("Multilingual agent started")
-    data = {
-        "inputs": "Amanda: I baked  cookies. Do you want some more?\r\nJerry: Sure!\r\nAmanda: I'll bring you tomorrow :-)"
-    }
+# # Creating the agent and funding it if necessary
+# agent = Agent(
+#     name="multilingual-agent",
+#     seed=SEED,
+#     port=8001,
+#     endpoint=["http://127.0.0.1:8001/submit"],
+# )
+# fund_agent_if_low(agent.wallet.address())
 
+class UAResponse(Model):
+    response: list
+
+class SummarizationRequest(Model):
+    url: str = Field(description="URL of the video you want to summarize")
+
+# Protocol declaration for UARequests
+multilingual_agent = Protocol("summary-request","0.1.0")
+
+# Declaration of a message event handler for handling UARequests and send respective response.
+@multilingual_agent.on_message(model=SummarizationRequest, replies={UAResponse, Error})
+async def handle_request(ctx: Context, sender: str, msg: SummarizationRequest):
+    # Logging the request information
+    ctx.logger.info(
+        f"Got request from  {sender} for summarization : {msg.url}")
+    
     try:
-        # Making POST request to Hugging Face SAMSUM BART API
-        response = requests.post(SAMSUM_BART_URL, headers=HEADERS, json=data)
+        transcript = get_video_script(msg.video_link)
+    except Exception as e:
+        await ctx.send(
+            sender,
+            UAgentResponse(
+                message=f"No transcript found for video! Error: {e}",
+                type=UAgentResponseType.ERROR,
+            ),
+        )
+        return
 
-        if response.status_code != 200:
-            # Error handling - send error message back to user if API call unsuccessful
-            await ctx.logger.error(f"Error: {response.json().get('error')}")
-            return
-        # If API call is successful, return the response from the model
-        model_res = response.json()[0]
-        ctx.logger.info(f"Response from model: {model_res}")
-        return
-    except Exception as ex:
-        # Catch and notify any exception occured during API call or data handling
-        await ctx.logger.error(f"An exception occurred while processing the request: {ex}")
-        return
+    summary = await summarize_transcript(transcript, msg.model)
+
+    print("Sending short article")
+    await ctx.send(
+        sender,
+        UAgentResponse(
+            message=f"Summary:\n{summary}",
+            type=UAgentResponseType.FINAL,
+        ),
+    )
+    return
+
+
+# Include protocol to the agent
+agent.include(multilingual_agent, publish_manifest=True)
 
 if __name__ == "__main__":
     agent.run()
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,6 +1,18 @@
 from urllib.parse import urlparse, parse_qs
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+import requests
+
+# The access token and URL for the SAMSUM BART model, served by Hugging Face
+HUGGING_FACE_ACCESS_TOKEN = os.getenv(
+    "HUGGING_FACE_ACCESS_TOKEN", "HUGGING FACE secret phrase :)")
+SAMSUM_BART_URL = "https://api-inference.huggingface.co/models/Samuela39/my-samsum-model"
+
+# Setting the headers for the API call
+HEADERS = {
+    "Authorization": f"Bearer {HUGGING_FACE_ACCESS_TOKEN}"
+}
 
 
 def get_video_script(url: str) -> list:
@@ -46,3 +58,34 @@ def chunk_text(text: list, chunk_size: int = 1000, chunk_overlap: int = 100) -> 
     texts = text_splitter.create_documents(text)
     
     return texts
+
+def get_summarization(text: str) -> str:
+    """
+    Summarize a string
+    """
+    data = {
+        "inputs": text
+    }
+    
+    response = requests.post(SAMSUM_BART_URL, headers=HEADERS, json=data)
+    model_res = response.json()[0]
+    summary = model_res['summary_text']
+    return summary
+    
+def summarize_transcript(text: list, chunk_size: int = 1000, chunk_overlap: int = 100) -> list:
+    """
+    Summarize a list of strings
+    """
+    chunks = chunk_text(text, chunk_size, chunk_overlap)
+    
+    summarized_chunks = []
+    
+    for chunk in chunks:
+        summarized_text = get_summarization(chunk)
+        summarized_chunks.append(summarized_text)
+    
+    summarized_text =''
+    for summary in summarized_chunks:
+        summarized_text += get_summarization(summary)
+    
+    return summarized_text
